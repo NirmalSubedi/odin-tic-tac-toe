b06816cb93f72c669760c316f930f51fd04a2b6b
Nirmal Subedi <101914003+NirmalSubedi@users.noreply.github.com>
2025-11-20 21:07:31 -0800

    Render board, player highlight, and game progress
    - Allows the game to be played on the browser by clicking start button.
    - Start a new match by clicking restart when the game has been started.
    - Current Player's turn is updated every play in h1 and by div
    highlighting.
    - Player name updated after an action (button click).

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const GameBoard = (function () {
     const board = [];
     let cellCount = 0;
 
-
     const winConditions = {
         rows: [],
         columns: [[], [], []],
@@ -13,7 +12,7 @@ const GameBoard = (function () {
         for (let i = 0; i < 3; i++) {
             board[i] = [];
             for (let j = 0; j < 3; j++) {
-                board[i].push({ value: ++cellCount });
+                board[i].push({ value: ++cellCount, mark: '' });
                 winConditions.columns[j].push(board[i][j]);
             }
             winConditions.rows.push(board[i]);
@@ -38,10 +37,6 @@ const GameBoard = (function () {
         console.table(displayBoard);
     };
 
-    // Make Array of win conditions (rows, columns, diagonals) 
-    // Fill cells with empty string
-    // Check for available cells (are cells empty string?)
-    // Reject placement of filled cell
     let validTarget = false;
 
     const targetCell = (cellNumber, player) => {
@@ -50,6 +45,7 @@ const GameBoard = (function () {
             for (let j = 0; j < 3; j++) {
                 if (board[i][j].value === cellNumber) {
                     board[i][j].value = player;
+                    board[i][j].mark = player;
                     validTarget = true;
                     return;
                 }
@@ -59,7 +55,8 @@ const GameBoard = (function () {
             printBoard();
             console.log('Invalid! Pick an available space from "1-9"');
         };
-    }
+    };
+
     const validateTarget = () => validTarget;
 
     const getBoard = () => board;
@@ -69,27 +66,30 @@ const GameBoard = (function () {
     const resetBoard = () => {
         cellCount = 0;
         winConditions.rows = [];
-        winConditions.columns = [[],[],[]];
-        winConditions.diagonals = [[],[]];
+        winConditions.columns = [[], [], []];
+        winConditions.diagonals = [[], []];
         board.forEach((row) => row.forEach((cell) => cell.value = cellCount));
         board.forEach((row) => row.pop());
         fillBoard();
     };
 
     return { getBoard, printBoard, getWinConditions, targetCell, validateTarget, resetBoard };
-
 })();
 
-function Players(playerOneName = 'Player One', playerTwoName = 'Player Two') {
-    // Create players' name and mark
+const players = (function (playerOneName = 'Player One', playerTwoName = 'Player Two') {
     const players = [
-        { name: playerOneName, mark: 'x' },
-        { name: playerTwoName, mark: 'o' },
+        {
+            name: playerOneName,
+            mark: 'X'
+        },
+        {
+            name: playerTwoName,
+            mark: 'O'
+        },
     ];
-    // Determine current player
+
     let currentPlayer = players[0];
 
-    // Switch current Player
     let switchCurrentPlayer = () => currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
 
     const getCurrentPlayer = () => currentPlayer;
@@ -97,17 +97,13 @@ function Players(playerOneName = 'Player One', playerTwoName = 'Player Two') {
     const getPlayers = () => players;
 
     return { switchCurrentPlayer, getCurrentPlayer, getPlayers };
-};
+})();
 
 const GameController = (function () {
     let board = GameBoard;
-    const players = Players();
     let winner = '';
-    let currentTurn = 1;
-
-    // Activate game round
-    let gameIsActive = true;
-
+    let turnsPlayed = 0;
+    let activeGame = true;
 
     const printNewRound = () => {
         board.printBoard();
@@ -115,7 +111,9 @@ const GameController = (function () {
     };
 
     const playRound = (cell) => {
+        if (activeGame === false) return;
         console.clear();
+
         board.targetCell(cell, players.getCurrentPlayer().mark);
 
         if (board.validateTarget() === false) return;
@@ -124,42 +122,155 @@ const GameController = (function () {
             Object.entries(board.getWinConditions())
                 .forEach(([key, keyValue]) => keyValue
                     .some((winLine) => (winLine
-                        .every((cell) => cell.value === players.getCurrentPlayer().mark))) ? gameIsActive = false : 'no win');
-            if (gameIsActive === false) {
+                        .every((cell) => cell.value === players.getCurrentPlayer().mark))) ? activeGame = false: 'no win');
+            if (activeGame === false) {
                 winner = players.getCurrentPlayer().name;
+                board.printBoard();
                 console.log(`${winner} wins!!!`)
             }
         }
 
         const checkDraw = () => {
-            if (winner === '' && currentTurn === 9) {
-                gameIsActive = false;
+            if (winner === '' && turnsPlayed === 9) {
+                activeGame = false;
                 board.printBoard();
                 console.log('DRAW! No winners...');
             }
         };
 
+        turnsPlayed++;
         checkWinner();
         checkDraw();
 
-        if (gameIsActive) {
+        if (activeGame) {
             console.log(`Marking cell ${cell} with ${players.getCurrentPlayer().name}'s "${players.getCurrentPlayer().mark}!"`);
             players.switchCurrentPlayer();
             printNewRound();
-            currentTurn++;
         };
     };
 
     const newMatch = () => {
         board.resetBoard();
-        gameIsActive = true;
+        activeGame = true;
         winner = '';
-        currentTurn = 1;
+        turnsPlayed = 0;
         players.switchCurrentPlayer();
         console.clear();
         printNewRound();
     }
+
+    const getWinner = () => winner;
+    const isGameActive = () => activeGame;
+    const getTurnsPlayed = () => turnsPlayed;
+
     printNewRound();
 
-    return { playRound, newMatch }
+    return { playRound, newMatch, getWinner, isGameActive, getTurnsPlayed }
+})();
+
+const DisplayController = (function () {
+    const messageH1 = document.querySelector('.message');
+    const boardDiv = document.querySelector('.board');
+    const startButton = document.querySelector('.start-btn');
+    const restartButton = document.querySelector('.restart-btn');
+    const playerInputs = document.querySelectorAll('.player-name');
+    const playerOneDiv = document.querySelector('.player-1');
+    const playerTwoDiv = document.querySelector('.player-2');
+
+    const board = GameBoard;
+    const game = GameController;
+
+    const removeCurrentPlayerRender = () => {
+        playerOneDiv.classList.remove('current-player');
+        playerTwoDiv.classList.remove('current-player');
+    };
+
+    const updateScreen = () => {
+        boardDiv.textContent = "";
+
+        const toggleCurrentPlayer = () => {
+            messageH1.textContent = `${players.getCurrentPlayer().name}'s turn...`
+            const isPlayerOneTurn = players.getCurrentPlayer().name === playerOneDiv.firstElementChild.value ||
+                players.getCurrentPlayer().name === playerOneDiv.firstElementChild.placeholder;
+
+            if (isPlayerOneTurn) {
+                playerOneDiv.classList.add('current-player');
+                playerTwoDiv.classList.remove('current-player');
+            } else {
+                playerTwoDiv.classList.add('current-player');
+                playerOneDiv.classList.remove('current-player');
+            }
+        };
+        toggleCurrentPlayer();
+
+        const renderBoard = () => {
+            board.getBoard().forEach((row) => {
+                row.forEach((cell) => {
+                    const cellButton = document.createElement("button");
+                    cellButton.classList.add("cell");
+                    cellButton.dataset.cellNumber = cell.value;
+                    cellButton.textContent = cell.mark;
+                    boardDiv.appendChild(cellButton);
+                })
+            });
+            boardDiv.appendChild(startButton);
+            boardDiv.appendChild(restartButton);
+        };
+        renderBoard();
+
+        const renderGameResults = () => {
+            // Win
+            if (game.isGameActive() === false && game.getWinner() !== '') {
+                messageH1.textContent = `${game.getWinner()} wins!`;
+                removeCurrentPlayerRender();
+            };
+            // Draw
+            if (game.getWinner() === '' && game.getTurnsPlayed() === 9) {
+                messageH1.textContent = `DRAW!`;
+                removeCurrentPlayerRender();
+            }
+
+        };
+        renderGameResults();
+
+    };
+
+    // Event Listeners and Handlers
+    const changePlayerNameHandler = (event) => {
+        const target = event.target;
+        const parent = target.parentElement;
+        const newName = target.value;
+        const playerNumber = Number(parent.getAttribute('class').at(-1));
+        players.getPlayers()[playerNumber - 1].name = newName === '' ? target.placeholder : newName;
+        updateScreen();
+    };
+    playerInputs.forEach((input) => input.addEventListener("change", changePlayerNameHandler));
+
+    const clickBoardHandler = (event) => {
+        const selectedCell = Number(event.target.dataset.cellNumber);
+        game.playRound(selectedCell);
+        updateScreen();
+    };
+
+    const clickStartHandler = () => {
+        boardDiv.addEventListener("click", clickBoardHandler);
+        startButton.classList.add('hide');
+        restartButton.classList.remove('hide');
+        updateScreen();
+    }
+    startButton.addEventListener('click', clickStartHandler);
+
+    const clickRestartHandler = (event) => {
+        event.stopPropagation();
+        game.newMatch();
+        const cells = document.querySelectorAll('.cell');
+        cells.forEach((cell) => cell.remove());
+        startButton.classList.toggle('hide');
+        restartButton.classList.toggle('hide');
+        messageH1.textContent = "Tic-Tac-Toe";
+        removeCurrentPlayerRender();
+        boardDiv.removeEventListener('click', clickBoardHandler);
+    }
+    restartButton.addEventListener('click', clickRestartHandler);
+
 })();
